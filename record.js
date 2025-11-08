bd4544a336e95803edc6f1f1659c020b70f79a39
leerainchen-tw <leerain.chen@gmail.com>
2025-11-08 22:56:55 +0800

    Add files via upload
    v20251022

---
+++ record.js
@@ -0,0 +1,28 @@
+module.exports = {
+   "checkin": [],
+   "checkin_count": 0,
+   "checking_rank": {},
+   "pending_checkin": {
+      "ad6271a0-ae13-45e4-969b-a94a239acb7f": {
+         "name": "陳里仁",
+         "timestamp": "2025-11-08 07:51:05",
+         "transaction_id": "ad6271a0-ae13-45e4-969b-a94a239acb7f",
+         "checkinType": "sport",
+         "verified": true
+      },
+      "eb5a0229-dded-4c87-a471-e8526c375bb0": {
+         "name": "陳里仁",
+         "timestamp": "2025-11-08 07:59:11",
+         "transaction_id": "eb5a0229-dded-4c87-a471-e8526c375bb0",
+         "checkinType": "sport",
+         "verified": true
+      },
+      "d0d40577-b80c-4d1a-864d-e141a51c979e": {
+         "name": "陳里仁",
+         "timestamp": "2025-11-08 08:06:48",
+         "transaction_id": "d0d40577-b80c-4d1a-864d-e141a51c979e",
+         "checkinType": "sport",
+         "verified": true
+      }
+   }
+}
---
+++ record.sample.js
@@ -0,0 +1,11 @@
+module.exports = {
+   "checkin": [
+      {
+        "nickname": "豆泥",
+        "message": "加油！",
+        "timestamp": "2024-04-02 12:00:00"
+      }
+   ],
+   "checkin_count": 1,
+   "checking_rank": {"豆泥": 1}
+}
